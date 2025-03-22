e2c88f3fc66a791b78e6b2d0ca33b9fb8b2e8039
Aman Bhatt <amanb2704@gmail.com>
2025-03-22 11:40:02 +0530

    multi threaded transaction test

--- app/src/test/java/org/foxdb/transaction/TransactionTest.java
+++ app/src/test/java/org/foxdb/transaction/TransactionTest.java
@@ -23,10 +23,10 @@ public class TransactionTest {
         FileManager fm = new FileManager(new File("./testdb"), 4000);
         LogManager lm = new LogManager(fm, "./testdb/test.log");
         BufferManager bm = new BufferManager(fm, lm, 10);
-        RecoveryManager rm = new RecoveryManager(lm, bm);
+        RecoveryManager rm = new RecoveryManager(lm, bm, fm);
         ConcurrencyManager cm = new ConcurrencyManager();
 
-        Transaction tx1 = new Transaction(cm, rm, bm, 1);
+        Transaction tx1 = new Transaction(cm, rm, bm,fm, 1);
         BlockID blk10 = new BlockID("./testdb/testfile", 10);
 
         tx1.insert(blk10, 0, "Aman".getBytes(StandardCharsets.UTF_8));
@@ -42,7 +42,7 @@ public class TransactionTest {
         iterateBlock(blk10, bm,rm);
 
 
-        Transaction tx2 = new Transaction(cm, rm, bm, 2);
+        Transaction tx2 = new Transaction(cm, rm, bm,fm, 2);
         tx2.insert(blk10, 0, "Aman".getBytes(StandardCharsets.UTF_8));
         tx2.insert(blk10, 1, "Bhatt".getBytes(StandardCharsets.UTF_8));
         tx2.update(blk10, 0, "Varadrajan".getBytes(StandardCharsets.UTF_8));
@@ -57,12 +57,6 @@ public class TransactionTest {
 
 
         /* only varadrajan left */
-
-
-
-
-
-
         try {
             FileUtils.deleteDirectory(new File("./testdb"));
         } catch (IOException e) {
@@ -87,6 +81,255 @@ public class TransactionTest {
             String s = new String(sp.get(i), StandardCharsets.UTF_8);
             System.out.println(s);
         }
+    }
+
+
+    @Test
+    public void TestMultiThreadWritersTransaction(){
+        BlockID blk10 = new BlockID("./testdb/testfile", 10);
+        FileManager fm = new FileManager(new File("./testdb"), 4000);
+        LogManager lm = new LogManager(fm, "./testdb/test.log");
+        BufferManager bm = new BufferManager(fm, lm, 10);
+        RecoveryManager rm = new RecoveryManager(lm, bm, fm);
+        ConcurrencyManager cm = new ConcurrencyManager();
+
+        Thread t = new Thread(()->{
+            Transaction tx1 = new Transaction(cm, rm, bm, fm,1);
+            tx1.insert(blk10, 0, "Aman".getBytes(StandardCharsets.UTF_8));
+            tx1.insert(blk10, 1, "Bhatt".getBytes(StandardCharsets.UTF_8));
+            tx1.update(blk10, 0, "Varadrajan".getBytes(StandardCharsets.UTF_8));
+            tx1.insert(blk10, 1, "Sunshine".getBytes(StandardCharsets.UTF_8));
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            tx1.commit();
+        });
+
+        Thread t2 = new Thread(()->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Transaction tx2 = new Transaction(cm, rm, bm, fm,2);
+            tx2.insert(blk10, 0, "Aman".getBytes(StandardCharsets.UTF_8));
+            tx2.insert(blk10, 1, "Bhatt".getBytes(StandardCharsets.UTF_8));
+            tx2.update(blk10, 0, "Varadrajan".getBytes(StandardCharsets.UTF_8));
+            tx2.remove(blk10, 1);
+            tx2.rollback();
+
+        });
+
+        t.start();
+        t2.start();
+
+
+        try {
+            t.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        iterateBlock(blk10, bm,rm);
+
+
+        iterateLogs(lm,rm);
+
+        try {
+            FileUtils.deleteDirectory(new File("./testdb"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    @Test
+    public void TestMultiThreadReadWriteTransaction(){
+        BlockID blk10 = new BlockID("./testdb/testfile", 10);
+        BlockID blk20 = new BlockID("./testdb/testfile", 20);
+        FileManager fm = new FileManager(new File("./testdb"), 4000);
+        LogManager lm = new LogManager(fm, "./testdb/test.log");
+        BufferManager bm = new BufferManager(fm, lm, 10);
+        RecoveryManager rm = new RecoveryManager(lm, bm, fm);
+        ConcurrencyManager cm = new ConcurrencyManager();
+
+        Thread t = new Thread(()->{
+            Transaction tx1 = new Transaction(cm, rm, bm, fm,1);
+            tx1.insert(blk10, 0, "Aman".getBytes(StandardCharsets.UTF_8));
+            tx1.insert(blk10, 1, "Bhatt".getBytes(StandardCharsets.UTF_8));
+            tx1.update(blk10, 0, "Varadrajan".getBytes(StandardCharsets.UTF_8));
+            tx1.insert(blk10, 1, "Sunshine".getBytes(StandardCharsets.UTF_8));
+            tx1.insert(blk20, 0, tx1.read(blk10, 1));
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            tx1.commit();
+        });
+
+        Thread t2 = new Thread(()->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Transaction tx2 = new Transaction(Transaction.Isolation.READ_UNCOMMITTED ,cm, rm, bm, fm,2);
+            byte[] b= tx2.read(blk10, 0);
+            byte[] b20 = tx2.read(blk20, 0);
+            System.out.println(new String(b, StandardCharsets.UTF_8) + " " + new String(b20, StandardCharsets.UTF_8));
+            tx2.commit();
+
+        });
+
+        t.start();
+        t2.start();
+
+
+        try {
+            t.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        iterateBlock(blk10, bm,rm);
+        iterateBlock(blk20, bm,rm);
+
+
+        iterateLogs(lm,rm);
+
+        try {
+            FileUtils.deleteDirectory(new File("./testdb"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    public void TestMultiThreadReadersTransaction(){
+        BlockID blk10 = new BlockID("./testdb/testfile", 10);
+        BlockID blk20 = new BlockID("./testdb/testfile", 20);
+        FileManager fm = new FileManager(new File("./testdb"), 4000);
+        LogManager lm = new LogManager(fm, "./testdb/test.log");
+        BufferManager bm = new BufferManager(fm, lm, 10);
+        RecoveryManager rm = new RecoveryManager(lm, bm, fm);
+        ConcurrencyManager cm = new ConcurrencyManager();
+
+        Thread t = new Thread(()->{
+            Transaction tx1 = new Transaction(cm, rm, bm,fm, 1);
+            tx1.insert(blk10, 0, "Aman".getBytes(StandardCharsets.UTF_8));
+            tx1.insert(blk10, 1, "Bhatt".getBytes(StandardCharsets.UTF_8));
+            tx1.update(blk10, 0, "Varadrajan".getBytes(StandardCharsets.UTF_8));
+            tx1.insert(blk10, 1, "Sunshine".getBytes(StandardCharsets.UTF_8));
+            tx1.insert(blk20, 0, tx1.read(blk10, 1));
+            tx1.commit();
+        });
+
+        Thread t2 = new Thread(()->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Transaction tx2 = new Transaction(cm, rm, bm,fm, 2);
+            byte[] b= tx2.read(blk10, 0);
+            byte[] b20 = tx2.read(blk20, 0);
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(new String(b, StandardCharsets.UTF_8) + " " + new String(b20, StandardCharsets.UTF_8));
+            tx2.commit();
+
+        });
+
+        Thread t3 = new Thread(()->{
+
+            Transaction tx2 = new Transaction(cm, rm, bm, fm, 3);
+            byte[] b= tx2.read(blk10, 0);
+            byte[] b20 = tx2.read(blk20, 0);
+
+
+            System.out.println(new String(b, StandardCharsets.UTF_8) + " " + new String(b20, StandardCharsets.UTF_8));
+            tx2.commit();
+
+        });
+
+
+
+
+
+        try {
+            t.start();
+            t.join();
+
+            t2.start();
+            t3.start();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        iterateBlock(blk10, bm,rm);
+        iterateBlock(blk20, bm,rm);
+
+
+        iterateLogs(lm,rm);
+
+        try {
+            FileUtils.deleteDirectory(new File("./testdb"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    public void SingleThreadEOFTest(){
+        FileManager fm = new FileManager(new File("./testdb"), 4000);
+        LogManager lm = new LogManager(fm, "./testdb/test.log");
+        BufferManager bm = new BufferManager(fm, lm, 10);
+        RecoveryManager rm = new RecoveryManager(lm, bm, fm);
+        ConcurrencyManager cm = new ConcurrencyManager();
+        BlockID blk10 = new BlockID("./testdb/testfile", 0);
+        Transaction tx1 = new Transaction(cm, rm, bm,fm, 1);
+        tx1.appendNewBlock("./testdb/testfile");
+        tx1.insert(blk10, 0, "Aman".getBytes(StandardCharsets.UTF_8));
+        tx1.insert(blk10, 1, "Bhatt".getBytes(StandardCharsets.UTF_8));
+        tx1.update(blk10, 0, "Varadrajan".getBytes(StandardCharsets.UTF_8));
+        tx1.insert(blk10, 1, "Sunshine".getBytes(StandardCharsets.UTF_8));
+        tx1.insert(blk10, 0, tx1.read(blk10, 1));
+        tx1.rollback();
+
+
+
+        bm.flushAll(1);
+
+        Transaction tx2 = new Transaction(Transaction.Isolation.SERIALIZABLE,cm, rm, bm, fm, 2);
+        System.out.println(tx1.fileLength("./testdb/testfile"));
+        tx2.commit();
+
+        iterateBlock(blk10, bm, rm);
+
+        try {
+            FileUtils.deleteDirectory(new File("./testdb"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void MultiThreadFileEOF(){
+
     }
 
 }
